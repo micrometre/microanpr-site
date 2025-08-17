8c1f6b01e380934c892755b6cc2d2c3afffb44b9
ubuntu <ubuntu@gmail.com>
2025-08-17 23:08:36 +0100

    sorting services menue

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -119,7 +119,7 @@ export default function Header() {
             />
 
             {/* Menu Panel */}
-            <div className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white dark:bg-gray-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10 dark:ring-white/10">
+            <div className="inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white dark:bg-gray-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10 dark:ring-white/10">
               <div className="flex items-center justify-between">
                 <button
                   type="button"
